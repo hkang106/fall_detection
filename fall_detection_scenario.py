332b0d943e526b8c68f21c9ffd75402b1929606b
hkang106 <hkang106@gmail.com>
2020-05-26 11:37:50 +0900

    fall detection scenario added

---
+++ fall_detection_scenario.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+import pandas as pd
+from PIL import Image
+import io
+import numpy as np
+import random
+import sys
+from fd_module import FallDetection, Model
+
+train1= pd.read_csv("body_posture_dataset/train_1.csv")
+train2= pd.read_csv("body_posture_dataset/train_2.csv")
+train3= pd.read_csv("body_posture_dataset/train_3.csv")
+
+
+filename1 = train1["filename"].tolist()
+xmin1 = train1["xmin"].tolist()
+ymin1 = train1["ymin"].tolist()
+xmax1 = train1["xmax"].tolist()
+ymax1 = train1["ymax"].tolist()
+
+filename2 = train2["filename"].tolist()
+xmin2 = train2["xmin"].tolist()
+ymin2 = train2["ymin"].tolist()
+xmax2 = train2["xmax"].tolist()
+ymax2 = train2["ymax"].tolist()
+
+filename3 = train3["filename"].tolist()
+xmin3 = train3["xmin"].tolist()
+ymin3 = train3["ymin"].tolist()
+xmax3 = train3["xmax"].tolist()
+ymax3 = train3["ymax"].tolist()
+
+stream = []
+for i, _ in enumerate(filename1):
+    temp = [filename1[i], [xmin1[i], ymin1[i], xmax1[i], ymax1[i]], 0] 
+    stream.append(temp)
+
+for i, _ in enumerate(filename2):
+    temp = [filename2[i], [xmin2[i], ymin2[i], xmax2[i], ymax2[i]], 2] 
+    stream.append(temp)
+
+for i, _ in enumerate(filename3):
+    temp = [filename3[i], [xmin3[i], ymin3[i], xmax3[i], ymax3[i]], 1] 
+    stream.append(temp)
+    
+#stream = np.asarray(stream)
+#random.seed(0)
+random.shuffle(stream)
+
+
+# inference
+
+model = Model()
+fd = FallDetection(20, 5, 7) #buffer size, fall_threshold, long_lie_threshold
+
+
+
+
+for item in stream:
+    image = item[0]
+    bbox = item[1]
+    label = item[2]
+    image = model.get_filepath(image, label)
+    image = model.preprocess_image(image, bbox)
+
+    # inference
+    prediction = model.inference(image)
+
+    # fall detection
+    fd.buffer_step(prediction)
+    if fd.detect_fall() and fd.detect_long_lie():
+        fd.generate_alarm()
---
+++ fd_module.py
@@ -0,0 +1,98 @@
+import sys
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+class Model:
+    def __init__(self):
+        # MODEL_PATH='model/basic-cnn-functional.h5'
+        # MODEL_PATH='model/basic-cnn-functional-expert.h5'
+        # MODEL_PATH='model/basic-cnn-sequential.h5'
+        self.MODEL_PATH = 'model/basic-cnn-sequential-expert.h5'
+        self.model = tf.keras.models.load_model(self.MODEL_PATH)
+        # inference를 할 경우엔 warning 무시
+        # training을 할 경우엔 compile 해야함
+
+    def inference(self, image):
+        prediction = self.model(image)
+        return np.argmax(prediction)
+
+    def get_filepath(self, filename, label):
+        return "body_posture_dataset/" + str(label) + "/" + filename
+
+    def preprocess_image(self, image, bbox):
+        img = Image.open(image).convert("L")  # convert to grayscale
+        img = img.crop(bbox)
+        img = img.resize((50, 50))
+        img = np.asarray(img) / 255.0
+        img = np.expand_dims(img, -1)
+        img = np.expand_dims(img, 0)
+
+        return img
+
+
+class FallDetection:
+    def __init__(self, buffer_size, fall_threshold, long_lie_threshold):
+        self.buffer_size = buffer_size
+        self.fall_threshold = fall_threshold
+        self.long_lie_threshold = long_lie_threshold
+
+        self.buffer = []
+        self.long_lie_window = []
+        self.lying_cnt = 0
+
+        self.STANDING = 0
+        self.LYING = 1
+        self.BENDING = 2
+
+    def buffer_step(self, label):
+        # self.lying_cnt = 0
+        self.buffer.append(label)
+
+        if len(self.buffer) > self.buffer_size:
+            self.buffer.pop(0)
+
+    def detect_fall(self):
+        # st: standing timestamp
+        # lt: lying timestamp
+        for st, label in enumerate(self.buffer):
+            if label == self.STANDING:
+                for lt in range(st, st + self.fall_threshold):
+                    if lt > len(self.buffer) - 1:
+                        break
+                    if self.buffer[lt] == self.LYING:
+                        self.st = st
+                        self.lt = lt
+                        return True
+
+        return False
+
+    def detect_long_lie(self):
+
+        self.lying_cnt = 0
+        self.long_lie_window = []
+
+        # 1. declaring sliding window
+        for t in range(self.lt, self.lt + self.long_lie_threshold):
+            if t > len(self.buffer) - 1:
+                break
+
+            self.long_lie_window.append(self.buffer[t])
+
+            # initiate lying count
+            if self.buffer[t] == self.LYING:
+                self.lying_cnt += 1
+
+        # alarm condition
+        if self.lying_cnt >= self.long_lie_threshold:
+            return True
+
+        else:
+            return False
+
+    def generate_alarm(self):
+        sys.stdout.write("[ALERT] fall-down has just occurred!\n")
+        sys.stdout.write("fall detected between " + str(self.st) + " and " + str(self.lt)+"\n")
+        sys.stdout.write("self.buffer: {} \n".format(self.buffer))
+        sys.stdout.write("--------\n")
+
